a1a8030282d9d3e27478fd98b64cc2c8f8473c6f
Gillou68310 <gilles_siberlin@hotmail.fr>
2014-06-25 16:39:10 +0200

    Sync core with upstream
    (cherry picked from commit beaa2f2d8c92156b6c67104a205af5c754525f62)

--- jni/mupen64plus-core/src/memory/memory.c
+++ jni/mupen64plus-core/src/memory/memory.c
@@ -37,13 +37,16 @@
 #include "flashram.h"
 
 #include "r4300/r4300.h"
-#include "r4300/macros.h"
+#include "r4300/cached_interp.h"
+#include "r4300/cp0.h"
 #include "r4300/interupt.h"
 #include "r4300/recomph.h"
 #include "r4300/ops.h"
+#include "r4300/tlb.h"
 
 #include "api/callbacks.h"
 #include "main/main.h"
+#include "main/profile.h"
 #include "main/rom.h"
 #include "osal/preproc.h"
 #include "plugin/plugin.h"
@@ -1188,7 +1191,7 @@ static void do_SP_Task(void)
                     {
 #ifdef DBG
                         if (lookup_breakpoint(0x80000000 + j * 0x10000, 0x10000,
-                                              BPT_FLAG_ENABLED |  BPT_FLAG_READ ) != -1)
+                                              M64P_BKP_FLAG_ENABLED | M64P_BKP_FLAG_READ) != -1)
                         {
                             readmem[0x8000+j] = read_rdram_break;
                             readmemb[0x8000+j] = read_rdramb_break;
@@ -1205,7 +1208,7 @@ static void do_SP_Task(void)
 #ifdef DBG
                         }
                         if (lookup_breakpoint(0xa0000000 + j * 0x10000, 0x10000,
-                                              BPT_FLAG_ENABLED |  BPT_FLAG_READ ) != -1)
+                                              M64P_BKP_FLAG_ENABLED | M64P_BKP_FLAG_READ) != -1)
                         {
                             readmem[0xa000+j] = read_rdram_break;
                             readmemb[0xa000+j] = read_rdramb_break;
@@ -1222,7 +1225,7 @@ static void do_SP_Task(void)
 #ifdef DBG
                         }
                         if (lookup_breakpoint(0x80000000 + j * 0x10000, 0x10000,
-                                              BPT_FLAG_ENABLED |  BPT_FLAG_WRITE ) != -1)
+                                              M64P_BKP_FLAG_ENABLED | M64P_BKP_FLAG_WRITE) != -1)
                         {
                             writemem[0x8000+j] = write_rdram_break;
                             writememb[0x8000+j] = write_rdramb_break;
@@ -1239,7 +1242,7 @@ static void do_SP_Task(void)
 #ifdef DBG
                         }
                         if (lookup_breakpoint(0xa0000000 + j * 0x10000, 0x10000,
-                                              BPT_FLAG_ENABLED |  BPT_FLAG_WRITE ) != -1)
+                                              M64P_BKP_FLAG_ENABLED | M64P_BKP_FLAG_WRITE) != -1)
                         {
                             writemem[0xa000+j] = write_rdram_break;
                             writememb[0xa000+j] = write_rdramb_break;
@@ -1263,9 +1266,9 @@ static void do_SP_Task(void)
 
         //gfx.processDList();
         rsp_register.rsp_pc &= 0xFFF;
-        start_section(GFX_SECTION);
+        timed_section_start(TIMED_SECTION_GFX);
         rsp.doRspCycles(0xFFFFFFFF);
-        end_section(GFX_SECTION);
+        timed_section_end(TIMED_SECTION_GFX);
         rsp_register.rsp_pc |= save_pc;
         new_frame();
 
@@ -1301,7 +1304,7 @@ static void do_SP_Task(void)
                     {
 #ifdef DBG
                         if (lookup_breakpoint(0x80000000 + j * 0x10000, 0x10000,
-                                              BPT_FLAG_ENABLED |  BPT_FLAG_READ ) != -1)
+                                              M64P_BKP_FLAG_ENABLED | M64P_BKP_FLAG_READ) != -1)
                         {
                             readmem[0x8000+j] = read_rdramFB_break;
                             readmemb[0x8000+j] = read_rdramFBb_break;
@@ -1318,7 +1321,7 @@ static void do_SP_Task(void)
 #ifdef DBG
                         }
                         if (lookup_breakpoint(0xa0000000 + j * 0x10000, 0x10000,
-                                              BPT_FLAG_ENABLED |  BPT_FLAG_READ ) != -1)
+                                              M64P_BKP_FLAG_ENABLED | M64P_BKP_FLAG_READ) != -1)
                         {
                             readmem[0xa000+j] = read_rdramFB_break;
                             readmemb[0xa000+j] = read_rdramFBb_break;
@@ -1335,7 +1338,7 @@ static void do_SP_Task(void)
 #ifdef DBG
                         }
                         if (lookup_breakpoint(0x80000000 + j * 0x10000, 0x10000,
-                                              BPT_FLAG_ENABLED |  BPT_FLAG_WRITE ) != -1)
+                                              M64P_BKP_FLAG_ENABLED | M64P_BKP_FLAG_WRITE) != -1)
                         {
                             writemem[0x8000+j] = write_rdramFB_break;
                             writememb[0x8000+j] = write_rdramFBb_break;
@@ -1352,7 +1355,7 @@ static void do_SP_Task(void)
 #ifdef DBG
                         }
                         if (lookup_breakpoint(0xa0000000 + j * 0x10000, 0x10000,
-                                              BPT_FLAG_ENABLED |  BPT_FLAG_WRITE ) != -1)
+                                              M64P_BKP_FLAG_ENABLED | M64P_BKP_FLAG_WRITE) != -1)
                         {
                             writemem[0xa000+j] = write_rdramFB_break;
                             writememb[0xa000+j] = write_rdramFBb_break;
@@ -1393,9 +1396,9 @@ static void do_SP_Task(void)
     {
         //audio.processAList();
         rsp_register.rsp_pc &= 0xFFF;
-        start_section(AUDIO_SECTION);
+        timed_section_start(TIMED_SECTION_AUDIO);
         rsp.doRspCycles(0xFFFFFFFF);
-        end_section(AUDIO_SECTION);
+        timed_section_end(TIMED_SECTION_AUDIO);
         rsp_register.rsp_pc |= save_pc;
 
         update_count();
--- jni/mupen64plus-core/src/r4300/recomp.c
+++ jni/mupen64plus-core/src/r4300/recomp.c
@@ -1,5 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   Mupen64plus - recomp.h                                                *
+ *   Mupen64plus - recomp.c                                                *
  *   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/           *
  *   Copyright (C) 2002 Hacktarux                                          *
  *                                                                         *
@@ -32,12 +32,15 @@
 #include "api/m64p_types.h"
 #include "api/callbacks.h"
 #include "memory/memory.h"
+#include "main/profile.h"
 
+#include "cached_interp.h"
 #include "recomp.h"
 #include "recomph.h" //include for function prototypes
-#include "macros.h"
+#include "cp0.h"
 #include "r4300.h"
 #include "ops.h"
+#include "tlb.h"
 
 static void *malloc_exec(size_t size);
 static void free_exec(void *ptr, size_t length);
@@ -2160,7 +2163,7 @@ void init_block(precomp_block *block)
 {
   int i, length, already_exist = 1;
   static int init_length;
-  start_section(COMPILER_SECTION);
+  timed_section_start(TIMED_SECTION_COMPILER);
 #ifdef CORE_DBG
   DebugMessage(M64MSG_INFO, "init block %x - %x", (int) block->start, (int) block->end);
 #endif
@@ -2344,7 +2347,7 @@ void init_block(precomp_block *block)
       init_block(blocks[(block->start-0x20000000)>>12]);
     }
   }
-  end_section(COMPILER_SECTION);
+  timed_section_end(TIMED_SECTION_COMPILER);
 }
 
 void free_block(precomp_block *block)
@@ -2369,7 +2372,7 @@ void free_block(precomp_block *block)
 void recompile_block(int *source, precomp_block *block, unsigned int func)
 {
    int i, length, finished=0;
-   start_section(COMPILER_SECTION);
+   timed_section_start(TIMED_SECTION_COMPILER);
    length = (block->end-block->start)/4;
    dst_block = block;
    
@@ -2497,7 +2500,7 @@ void recompile_block(int *source, precomp_block *block, unsigned int func)
    fclose(pfProfile);
    pfProfile = NULL;
 #endif
-   end_section(COMPILER_SECTION);
+   timed_section_end(TIMED_SECTION_COMPILER);
 }
 
 static int is_jump(void)
