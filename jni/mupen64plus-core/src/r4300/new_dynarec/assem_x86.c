d9fd02fa5c8df86852e77ac0540344da0b036873
Gillou68310 <gilles_siberlin@hotmail.fr>
2014-10-31 11:38:22 +0100

    Dynarec: Fixed DADD/DADDU/DSUB/DSUBU
    DADD/DADDU was broken on both x86 and ARM:
    The carry from the low 32bit addition was not propagated when adding the
    high 32bit part, see:
    
    http://stackoverflow.com/questions/1652654/adding-64-bit-numbers-using-32-bit-arithmetic
    
    DSUB/DSUBU was broken on x86 only.
    
    (cherry picked from commit b2325097fee0535a127ac58390f4576b8ba3ec02)

--- jni/mupen64plus-core/src/r4300/new_dynarec/assem_x86.c
+++ jni/mupen64plus-core/src/r4300/new_dynarec/assem_x86.c
@@ -793,15 +793,42 @@ static void emit_add(int rs1,int rs2,int rt)
     output_byte(0x01);
     output_modrm(3,rs2,rs1);
   }else {
-    assem_debug("lea (%%%s,%%%s),%%%s",regname[rs1],regname[rs2],regname[rt]);
-    output_byte(0x8D);
-    if(rs1!=EBP) {
-      output_modrm(0,4,rt);
-      output_sib(0,rs2,rs1);
-    }else if(rs2!=EBP) {
-      output_modrm(0,4,rt);
-      output_sib(0,rs1,rs2);
+    if((rs1!=EBP)||(rs2!=EBP)) {
+     assem_debug("mov %%%s,%%%s",regname[rs1],regname[rt]);
+     output_byte(0x89);
+     output_modrm(3,rt,rs1);
+     assem_debug("add %%%s,%%%s",regname[rs2],regname[rt]);
+     output_byte(0x01);
+     output_modrm(3,rt,rs2);
     }else /* lea 0(,%ebp,2) */{
+      output_byte(0x8D);
+      output_modrm(0,4,rt);
+      output_sib(1,EBP,5);
+      output_w32(0);
+    }
+  }
+}
+
+static void emit_adc(int rs1,int rs2,int rt)
+{
+  if(rs1==rt) {
+    assem_debug("adc %%%s,%%%s",regname[rs2],regname[rs1]);
+    output_byte(0x11);
+    output_modrm(3,rs1,rs2);
+  }else if(rs2==rt) {
+    assem_debug("adc %%%s,%%%s",regname[rs1],regname[rs2]);
+    output_byte(0x11);
+    output_modrm(3,rs2,rs1);
+  }else {
+    if((rs1!=EBP)||(rs2!=EBP)) {
+     assem_debug("mov %%%s,%%%s",regname[rs1],regname[rt]);
+     output_byte(0x89);
+     output_modrm(3,rt,rs1);
+     assem_debug("adc %%%s,%%%s",regname[rs2],regname[rt]);
+     output_byte(0x11);
+     output_modrm(3,rt,rs2);
+    }else /* lea 0(,%ebp,2) */{
+      output_byte(0x8D);
       output_modrm(0,4,rt);
       output_sib(1,EBP,5);
       output_w32(0);
@@ -1150,6 +1177,21 @@ static void emit_addimm64_32(int rsh,int rsl,int imm,int rth,int rtl)
     emit_mov(rsh,rth);
     emit_mov(rsl,rtl);
     emit_addimm64_32(rth,rtl,imm,rth,rtl);
+  }
+}
+
+static void emit_sbc(int rs1,int rs2,int rt)
+{
+  if(rs1==rt) {
+    assem_debug("sbb %%%s,%%%s",regname[rs2],regname[rs1]);
+    output_byte(0x19);
+    output_modrm(3,rs1,rs2);
+  } else if(rs2==rt) {
+    emit_neg(rs2,rs2);
+    emit_adc(rs2,rs1,rs2);
+  } else {
+    emit_mov(rs1,rt);
+    emit_sbc(rt,rs2,rt);
   }
 }
 
